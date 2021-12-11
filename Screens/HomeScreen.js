838eb54d5190d9f3d16cbb46e62be0aa2f769aa5
acofrin <acofrin@wisc.edu>
2021-12-11 13:04:09 -0800

    adding moiuntain background

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet, ImageBackground} from 'react-native';
 import MainScreenButton from '../buttons/MainScreenButton';
 import Colors from '../constants/Color';
 
 const HomeScreen = props => {
     return (
         <View style={styles.screen}>
+            <ImageBackground source = {require('../assets/Mount.png')} resizeMode="cover" style={styles.image}>
             <MainScreenButton style={styles.profile} title="Profile"/>
             <MainScreenButton style={styles.viewClimbs} title="View Climbs"/>
             <MainScreenButton style={styles.logClimbs} title="Log Climbs"/>
+            </ImageBackground>
         </View>
     );
 };
@@ -29,7 +31,11 @@ const styles = StyleSheet.create({
     },
     logClimbs: {
         backgroundColor: Colors.accent2
-    }
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center"
+      }
 });
 
 export default HomeScreen;
