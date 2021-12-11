5dc7347cf5bb01830456c5082e91727bd8bcad81
acofrin <acofrin@wisc.edu>
2021-12-11 13:50:56 -0800

    log screen text input?
    copies from other thing... i dont actually know what props do lol

--- Screens/LogScreen.js
+++ Screens/LogScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
-const LogScreen = props => {};
+const LogScreen = props => {
+    return <TextInput  {...props} style={{...styles.input, ...props.style }}/>
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        height: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        marginVertical: 10
+    },
+});
 
 export default LogScreen;
