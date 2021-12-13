24c16532e7983ad05bb7c929ae2704a5110fd892
acofrin <acofrin@wisc.edu>
2021-12-12 19:24:54 -0800

    log screen

--- Screens/LogScreen.js
+++ Screens/LogScreen.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
-import Colors from '../constants/Color';
+import { StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native';
+import Color from '../constants/Color';
+import Texts from '../constants/Text';
 
 const LogScreen = props => {
     return (
-        <View>
-            <Text> Date </Text>
-            <Text> Tick </Text>
-            <Text> Type </Text>
-            <Text> Style </Text>
-            <Text> Where </Text>
-            <Text> Notes </Text>
+        <View style={styles.screen}>
+            <View style={styles.sidebyside}>
+                <Text style= {Texts.average, styles.headerLabels} > Date     </Text>
+                <TextInput style= {styles.input}/>
+            </View>
+                <Text style= {Texts.average, styles.headerLabels}> Tick </Text>
+                <Text style= {Texts.average, styles.headerLabels}> Type </Text>
+                <Text style= {Texts.average, styles.headerLabels}> Style </Text>
+                <Text style= {Texts.average, styles.headerLabels}> Where </Text>
+                <Text style= {Texts.average, styles.headerLabels}> Notes </Text>
+                <TouchableOpacity style={styles.buttonContainer}>
+                     <Text style= {Texts.button} >Log It!</Text>
+                </TouchableOpacity> 
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignContent: 'flex-start'
+    },
+    buttonContainer: {
+        flex:1,
+        elevation: 8,
+        backgroundColor: Color.accent2,
+        borderRadius: 10,
+        borderStyle: 'solid',
+        borderColor: Color.accent3,
+        borderWidth: 2,
+        marginVertical:200,
+        marginHorizontal:10,
+        paddingHorizontal: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        elevation: 5,
+        shadowOpacity: 0.86,  
+    },
     input: {
         height: 30,
         borderBottomWidth: 1,
-        borderBottomColor: 'grey',
+        borderBottomColor: 'black',
         marginVertical: 10
+    },
+    headerLabels: {
+        justifyContent: "flex-start",
+        alignContent: "flex-start"
+    },
+    sidebyside:{
+        flexDirection: 'row',
+        alignItems: 'center'
     },
 });
 
--- constants/Text.js
+++ constants/Text.js
@@ -13,5 +13,10 @@ export default {
         color: Colors.primary,
         alignSelf: "center",
         fontFamily: "Moon2.0-Light",
+    },
+    average: {
+        fontSize: 18,
+        color: Colors.accent3,
+        alignSelf: "center",
     }
 }
